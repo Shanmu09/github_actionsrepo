c808580609ab66d78ef4cf7e5e57a59a683cac91
Shanmu09 <shanmu0909@gmail.com>
2024-06-18 09:27:33 +0200

    update

--- src/main/java/com/example/springboot_cicd/SpringbootCicdApplication.java
+++ src/main/java/com/example/springboot_cicd/SpringbootCicdApplication.java
@@ -2,10 +2,15 @@ package com.example.springboot_cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class SpringbootCicdApplication {
 
+	@GetMapping("/message")
+	public String message() {
+		return "Hello World";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCicdApplication.class, args);
 	}
